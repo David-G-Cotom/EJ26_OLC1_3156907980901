bd96ee10678b44e60aada31994f26bb08f32a96d
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2026-06-04 19:56:28 -0600

    Project folder created using NetBeans

---
+++ proyecto_compi1_vj26/src/main/java/com/mycompany/proyecto_compi1_vj26/Proyecto_compi1_vj26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_compi1_vj26;
+
+/**
+ *
+ * @author david
+ */
+public class Proyecto_compi1_vj26 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
